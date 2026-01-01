c4d9a2a69a86f65204ebbe9b2e850d769a8b19d6
istiak.ovi <istiak.ovi@technonext.com>
2026-01-01 18:45:55 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService, ApiResponse } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'PharmacyManagement';
+  name = 'Angular Developer';
+  testResponse = signal<ApiResponse<string> | null>(null);
+
+  constructor(private api: ApiService) {
+    this.loadTest();
+  }
+
+  loadTest() {
+    this.api.getTest().subscribe({
+      next: (res) => this.testResponse.set(res),
+      error: (err) => console.error(err)
+    });
+  }
 }
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+export interface ApiResponse<T> {
+  isSuccess: boolean;
+  statusCode: number;
+  status: string;
+  message: string;
+  data: T;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+   // CALL YOUR TEST API
+  getTest(): Observable<ApiResponse<string>> {
+    return this.http.get<ApiResponse<string>>(`${this.baseUrl}/Test`);
+  }
+}
